2156db0067819bb2ed432896a000242cf83a7ba2
youssefmiftah <miftahyo@outlook.fr>
2021-12-04 12:49:50 +0100

    day 4

---
+++ day_4.py
@@ -0,0 +1,68 @@
+#%%
+import numpy as np
+
+class Board(object):
+    def __init__(self, np_array, tag):
+        self.table = np_array
+        self.checked = np.zeros_like(np_array)
+        self.won = False
+        self.tag = tag
+        self.score = 0
+
+    def _check_number(self, number):
+        self.checked[self.table == number] = 1
+
+    def _is_game_over(self):
+        horizontal = np.prod(self.checked, axis=1)
+        vertical = np.prod(self.checked, axis=0)
+        return np.any(horizontal) or np.any(vertical)
+
+    def draw_number(self, number):
+        self._check_number(number)
+        if self._is_game_over():
+            self.won = True
+            self.score =  np.sum(self.table * (1 - self.checked)) * number
+            return self.score
+        return 0
+
+
+class BingoGame(object):
+    def __init__(self, file_path):
+        with open(file_path) as file:
+            lines=file.readlines()
+        self.plays = np.fromstring(lines[0].strip(), dtype=np.int32, sep=',')
+
+        a = ''.join(lines[2:])
+        tables = np.fromstring(a.replace('\n', ' '), sep=' ', dtype=np.int32).reshape((-1,5))
+        X,Y = tables.shape
+        self.n_tables = int(X/5)
+        tables = [tables[i:i+5, :] for i in range(0, X, 5)]
+        self.boards = [Board(array, i+1) for i, array in enumerate(tables)]
+
+    def play_game(self):
+        for p in self.plays:
+            for i, b in enumerate(self.boards):
+                score = b.draw_number(p)
+                if score > 0:
+                    return i, score
+
+    def play_to_lose(self):
+        n_in_play = self.n_tables
+        for p in self.plays:
+            for b in (b for b in self.boards if not b.won):
+                score = b.draw_number(p)
+                if score > 0:
+                    n_in_play -= 1
+                if n_in_play == 0:
+                    return b.tag, score
+#%%
+print('PART 1')
+board, score = BingoGame('day_4_input.txt').play_game()
+print('Winner is board {} with score {}'.format(board+1, score))
+
+# %%
+
+print('PART 2')
+board, score = BingoGame('day_4_input.txt').play_to_lose()
+print('Loser is board {} with score {}'.format(board+1, score))
+# %%
